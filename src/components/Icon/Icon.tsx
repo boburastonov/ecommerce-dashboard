8ce21ca6389920a1ea949bbc540cb141b40757e8
boburastonov <boburastonov33@gmail.com>
2025-08-13 10:11:46 +0500

    Add Icon component

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import React from "react";
+import cx from "classnames";
+import get from "lodash/get";
+
+import * as List from "./list";
+
+import cls from "./Icon.module.scss";
+
+export type Type = "myicons" | "flag" | "fluent" | "sox";
+
+export type Variant = "regular" | "custom" | "filled" | "bold";
+
+export type Color = "#000000" | "#ffffff" | "#2d4191" | "#f03a45" | "#39bd83";
+
+interface IProps {
+  name: string | any;
+  type?: Type;
+  variant?: Variant;
+  size?: number;
+  className?: string;
+  color?: Color;
+  onClick?: () => void;
+}
+
+const Icon: React.FC<IProps> = ({
+  name,
+  type = "myicons",
+  variant = "regular",
+  size,
+  className,
+  color,
+  onClick,
+}) => {
+  const Component = get(List, `[${type}][${variant}][${name}]`);
+
+  if (!Component) {
+    console.log("Icon component not found");
+    return null;
+  }
+
+  return (
+    <div
+      className={cx(cls.wrapper, className)}
+      {...{ onClick }}
+      style={
+        {
+          ...(size ? { "--size": `${size}px` } : {}),
+          color,
+        } as React.CSSProperties
+      }
+    >
+      <Component />
+    </div>
+  );
+};
+
+export default Icon;
